dd2a05141d2e577e0a55b1966aaa5a05a6df8982
Sarah M Brown <brownsarahm@uri.edu>
2022-09-13 11:39:25 -0400

    initial setup and prelim

---
+++ conf.py
@@ -0,0 +1,127 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Teaching Data Science Ethics'
+copyright = '2022, Sarah M Brown'
+author = 'Sarah M Brown'
+
+# The short X.Y version
+version = '2022.09.10'
+
+# The full version, including alpha/beta/rc tags
+release = '2022.09.10'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_nb",
+    "ablog",
+    'sphinx.ext.intersphinx',
+    "sphinx_panels",
+    "sphinxcontrib.bibtex",
+    "sphinxext.opengraph",
+    'hieroglyph',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates', '_build', 'Thumbs.db', '.DS_Store', "*import_posts*",
+        "**/pandoc_ipynb/inputs/*", "README.md", '_bibliography',
+        '_data','_pages','_people','_projects']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_theme_options = {
+  "github_url": "https://github.com/brownsarahm/",
+  "twitter_url": "https://twitter.com/brownsarahm",
+  "search_bar_text": "Search this site...",
+  "google_analytics_id": "",
+  "navbar_end": ["search-field.html", "navbar-icon-links"],
+}
+
+# -- Extension configuration -------------------------------------------------
+
+
+# Panels config
+panels_add_bootstrap_css = False
+
+# MyST config
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    # "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    # "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+# Bibliography and citations
+bibtex_bibfiles = ["_static/references.bib"]
+
+
+nb_render_priority = {
+  "html": (
+            "application/vnd.jupyter.widget-view+json",
+            "application/javascript",
+            "text/html",
+            "image/svg+xml",
+            "image/png",
+            "image/jpeg",
+            "text/markdown",
+            "text/latex",
+            "text/plain",
+        ),
+  "slides": (
+            "application/vnd.jupyter.widget-view+json",
+            "application/javascript",
+            "text/html",
+            "image/svg+xml",
+            "image/png",
+            "image/jpeg",
+            "text/markdown",
+            "text/latex",
+            "text/plain",
+        )
+}
---
+++ index.md
@@ -0,0 +1,112 @@
+# Incorporating Ethics into Data Science Courses
+
+
+```{slide} Warm Up
+:level: 2
+
+Is your work in data science ethical?
+```
+
+
+```{ifnotslides}
+How did you learn to do that?
+```
+
+
+```{slide}
+
+Did you have formal data ethics training?
+
+Did you have formal discipline based ethics training?
+```
+
+```{slide}  Where we are going
+
+
+I want you to leave with:
+
+* something small enough that you can do it this semester
+
+* something you can implement next semester
+
+* ideas so that you can develop bigger ideas over time
+
+
+```
+
+
+
+```{slide} How we will do that
+
+- About me
+- Goals
+- Examples
+
+```
+
+## About Me
+
+```{slide} I am an engineer
+
+- professionalized
+- ABET accreditation standardizes somewhat
+```
+
+
+## Engineering Design process
+
+<!-- diagram -->
+
+
+```{slide}
+Building intuition
+```
+<!-- no calculator exam -->
+
+
+
+```{slide} Applications
+
+- Human subject research at MGH Breast Imaging Research
+- BAE Systems Target Development Lab
+- Human subject research at C.S. Draper Laboratory
+- ML Lab with many collaborators: psyhologists, civil engineers, physician scientists
+```
+
+
+
+```{slide} Training in Instruction and Design
+
+```
+
+```{slide} FAccT Scholarship
+```
+
+```{slide}
+> Data science ethics is not, and cannot be, a *thing* the way that biomedical ethics or legal ethics, or even engineering ethics are *things*.
+
+```
+
+```{slide}
+## General  Framework
+
+- safety through correctness/ robustness
+- consider points of failure
+- incorporate context
+
+```
+
+```{slide}
+## Wrap up
+
+- What is one thing you will do this semester?
+- What is one thing you want to learn more about?
+
+```
+
+
+```{ifnotslides}
+
+For data science ethics education to change the course of data science practice outside of the academy, ethics must be integrated throughout a student's education and deeply connected to practical computational choices. This session will equip each attendee with small changes that can be made in the current semester and a framework for broader course revisions to deeply integrate ethical thinking into their data science courses.  
+
+```
